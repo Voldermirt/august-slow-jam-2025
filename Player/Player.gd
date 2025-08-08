92fc30931bb5a003d9a1e1f1caca666368ae6f53
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2025-08-08 13:18:20 -0700

    Added a weapon

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 class_name PlayerCharacter2D
 
+@onready var weapon: PlayerWeapon = $Weapon
+
 var moving_speed: float = 500
 
+func _weapon_rotation_process():
+	weapon.look_at(get_global_mouse_position())
+
 func _player_movement_process():
-	var direction = Vector2.ZERO
+	var direction: Vector2 = Vector2.ZERO
 
 	if Input.is_action_pressed("player_move_right"):
 		direction.x += 1
@@ -24,3 +29,4 @@ func _player_movement_process():
 
 func _physics_process(delta):
 	_player_movement_process()
+	_weapon_rotation_process()
