1e85ff50dfc9086e32d17b0ad28f1fbe1148295b
Voldermirt <emars.nighting@gmail.com>
2025-08-08 22:47:13 -0700

    Added zooming out to the desk view

---
+++ scripts/main_room.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var zoom_anim := $ZoomAnim
+
+var zoom_out = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("zoom") and not zoom_out:
+		#$ZoomAnim.pause()
+		zoom_out = true
+		$ZoomAnim.play("zoom")
+	elif event.is_action_released("zoom") and zoom_out:
+		#$ZoomAnim.pause()
+		zoom_out = false
+		$ZoomAnim.play_backwards("zoom")
