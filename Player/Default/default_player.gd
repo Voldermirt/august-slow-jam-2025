f4ab0cf89604c3c6fc4199d96caef5fcfa86d8c4
Voldermirt <emars.nighting@gmail.com>
2025-08-10 17:12:33 -0700

    Added some sort of Sword mechanics for the Default player

--- Player/Default/default_player.gd
+++ Player/Default/default_player.gd
@@ -3,8 +3,9 @@ extends BasePlayer2D
 # The default player game starts with
 class_name DefaultPlayer2D
 
-@onready var default_weapon: DefaultWeapon2D = $DefaultWeapon2D
+@onready var default_weapon: DefaultSword = $DefaultSword
 
 func _physics_process(delta):
 	_player_movement_process()
-	_weapon_rotation_process(default_weapon)
+	if not default_weapon.is_attacking():
+		_weapon_rotation_process(default_weapon)
--- Player/Default/default_weapon.gd
+++ Player/Default/default_weapon.gd
@@ -1,3 +1,35 @@
 extends BaseWeapon2D
+class_name DefaultSword
 
-class_name DefaultWeapon2D
+@onready var anim = $Rotatator/AnimationPlayer
+@onready var block_col = $Rotatator/BlockCollision
+
+func is_attacking():
+	return anim.current_animation == "swing"
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("primary"):
+		attack()
+	if Input.is_action_just_pressed("secondary"):
+		block()
+
+func attack():
+	anim.play("swing")
+
+func block():
+	anim.play("block")
+	block_col.process_mode = Node.AUTO_TRANSLATE_MODE_INHERIT
+
+func reset():
+	anim.play("idle")
+	block_col.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name in ["block", "swing"]:
+		reset()
+
+func _on_sword_body_entered(body: Node2D) -> void:
+	if not is_attacking():
+		return
+	# Handle attack
+	# To be implemented once enemies exist I guess
