bc6db57918b53ab1b1bdac85d71ed4babeac1412
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2025-08-13 14:39:05 -0700

    enemy prototype

--- Collectables/base_collectable.gd
+++ Collectables/base_collectable.gd
@@ -2,13 +2,17 @@ extends Area2D
 
 class_name BaseCollectable2D
 
+const BASE_VALUE = 1
+
+func get_value():
+	return BASE_VALUE
+
 func _ready():
 	
 	if not get_parent().is_in_group("switch_wrapper"):
 		assert(false, str(self, " object is not the child of in the SwitchWrapper"))
 		
 	body_entered.connect(_on_body_entered)
-
 
 func _on_body_entered(body: CharacterBody2D):
 	if body is BasePlayer2D:
